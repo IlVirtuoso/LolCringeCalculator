f01764725463fb3abca84503c2887880ee5fbb8f
Virtuoso <Virtuoso@DESKTOP-K7318QU>
2019-11-24 21:20:14 +0100

    Aggiungere i file di progetto.

---
+++ LolCringeCalculator/Resources/Cringe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
+
+namespace LolCringeCalculator.Resources
+{
+    class Cringe
+    {
+        System.Timers.Timer timer;
+
+        public Cringe(int waveInterval)
+        {
+            minion SiegeMinion = new minion(1, 60);
+            minion MeleeMinion = new minion(3, 21);
+            minion CasterMinion = new minion(3, 14);
+            timer.Elapsed += Wave;
+        }
+
+        private void Wave(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void start()
+        {
+            timer.Start();
+        }
+
+        public void change(int level)
+        {
+            switch (level)
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+
+
+                    break;
+
+
+                case 3:
+
+
+                    break;
+
+                case 4:
+
+
+                    break;
+            }
+        }
+    }
+}
---
+++ LolCringeCalculator/Resources/minion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace LolCringeCalculator.Resources
+{
+    class minion
+    {
+        private int _armour;
+        public int armour
+        {
+            get { return _armour; }
+            set { _armour = value; }
+        }
+
+        private int _MR;
+
+        public int MagicResistance
+        {
+            get { return _MR; }
+            set { MagicResistance = value; }
+        }
+
+        private int _healt;
+
+        public int healt
+        {
+            get { return _healt; }
+            set { _healt = value; }
+        }
+
+        private int _AD;
+        
+        public int Ad
+        {
+            get { return _AD; }
+            set { _AD = value; }
+        }
+
+        private int _AP;
+
+        public int Ap
+        {
+            get { return _AP;  }
+            set { _AP = value}
+        }
+
+        private int goldValue;
+
+        public int spawned;
+
+        private int maxnumber;
+
+
+        private int actuals;
+
+        public minion(int maxnumber, int goldValue)
+        {
+            this.maxnumber = maxnumber;
+            this.goldValue = goldValue;
+        }
+
+        public int kill()
+        {
+            actuals = 0;
+            return this.goldValue * maxnumber;
+        }
+
+        public void spawn()
+        {
+            actuals = maxnumber;
+            spawned = spawned + actuals;
+        }
+
+        public void setGold(int goldValue)
+        {
+            this.goldValue = goldValue;
+        }
+    }
+}
+
+ 
+
